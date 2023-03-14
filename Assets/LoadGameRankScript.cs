4cf3170a4791d5bfe57236490093c8f5471fc649
Burak HOŞGÖR <burak_hosgor@hotmail.com>
2023-03-14 21:07:47 +0300

    Oyun menüsü Yapıldı

---
+++ Assets/LoadGameRankScript.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Unity.IO;
+
+public class LoadGameRankScript : MonoBehaviour
+{
+    public Text BestPlayerName;
+    
+    
+
+    private static int BestScore;
+    private static int BestPlayer;
+
+
+    private void Awake()
+    {
+        LoadGameRank();
+
+    }
+
+    private void SetBestPlayer()
+    {
+        if (BestPlayer == null && BestScore == 0)
+        {
+            BestPlayerName.text = "";
+        }
+        else
+        {
+            BestPlayerName.text = $"Best Score-:{BestScore}";
+            
+
+        }
+    }
+
+    public void LoadGameRank()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            BestPlayer = data.TheBestPlayer;
+            BestScore = data.HighiestScore;
+            SetBestPlayer();
+        }
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public int HighiestScore;
+        public int TheBestPlayer;
+    }
+
+}
---
+++ Assets/PlayerDataHandle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDataHandle : MonoBehaviour
+{
+    public static PlayerDataHandle Instance;
+    public string PlayerName;
+    public int Score;
+
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
